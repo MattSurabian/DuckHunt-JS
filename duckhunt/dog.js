ca34ea8e694d9e15e80fa995d5c49f43ec6acc09
Matt Surabian <matt@mattsurabian.com>
2013-01-26 21:41:02 -0500

    Dogs should append to the DOM on init

--- duckhunt/dog.js
+++ duckhunt/dog.js
@@ -1,19 +1,21 @@
 function Dog(id, game){
     this.id = id;
     this.game = game; // game object to trigger events on and append to
-    this.dog = $(id);
 
-    this.dog.hide();
+    $('<div id="'+this.id+'" class="theDog"></div>').appendTo(this.game);
+    this.DOM = $("#"+this.id);
+
+    this.DOM.hide();
 }
 
 /**
  * Used at the start of a level
  */
 Dog.prototype.intro = function(){
-    this.dog.addClass('searchDog');
-    this.dog.show(); // HA GET IT DOG SHOW!?!
-    this.dog.sprite({fps: 6, no_of_frames: 4});
-    this.dog.animate({
+    this.DOM.addClass('searchDog');
+    this.DOM.show(); // HA GET IT DOG SHOW!?!
+    this.DOM.sprite({fps: 6, no_of_frames: 4});
+    this.DOM.animate({
         left: '240'
     },5000,'linear',function(){
         $(this).destroy();
@@ -41,8 +43,8 @@ Dog.prototype.intro = function(){
  * Used when duck is shot and the dog retrieves
  */
 Dog.prototype.fetch = function(){
-    this.dog.show(); // HA GET IT DOG SHOW!?!
-    this.dog.css("background-position","0px 0px");
+    this.DOM.show(); // HA GET IT DOG SHOW!?!
+    this.DOM.css("background-position","0px 0px");
     this._upDown();
 };
 
@@ -50,14 +52,14 @@ Dog.prototype.fetch = function(){
  * Used when ducks are missed
  */
 Dog.prototype.laugh = function(){
-    this.dog.stop(true,false);
-    this.dog.show();
-    this.dog.css("background-position","-276px 0px");
+    this.DOM.stop(true,false);
+    this.DOM.show();
+    this.DOM.css("background-position","-276px 0px");
     this._upDown();
 };
 
 Dog.prototype._upDown = function(){
-    return this.dog.animate({
+    return this.DOM.animate({
         bottom: '110'
     },400,function(){
         setTimeout(_.bind(function(){
