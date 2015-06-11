f10d98a9268ae86739be3ca794673605ddf11cca
Matt Surabian <matt@mattsurabian.com>
2015-06-11 00:18:16 -0400

    Don't allow the dev task to exit on jshint errors

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,21 @@ var jshint = require('gulp-jshint');
 var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 
+// Command line option:
+//  --fatal=[warning|error|off]
+var fatalLevel = require('yargs').argv.fatal;
+
+var ERROR_LEVELS = ['error', 'warning'];
+
+function isFatal(level) {
+  return ERROR_LEVELS.indexOf(level) <= ERROR_LEVELS.indexOf(fatalLevel || 'error');
+}
+
 // Handle an error based on its severity level.
 // Log all levels, and exit the process for fatal levels.
 function handleError(level, error) {
   gutil.log(gutil.colors.red(error.message));
-  if (level === 'error') {
+  if (isFatal(level)) {
     process.exit(1);
   }
 }
@@ -48,6 +58,10 @@ gulp.task('libs', function() {
 });
 
 gulp.task('dev', function() {
+  // no fatal errors during active development by default
+  // this prevents this task from exiting unexpectedly
+  fatalLevel = fatalLevel || 'off';
+
   livereload.listen();
   gulp.watch('duckhunt/*.js', ['duckhunt']);
   gulp.watch('lib/*.js', ['libs']);
