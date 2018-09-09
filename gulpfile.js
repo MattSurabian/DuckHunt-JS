b29419aa9acc4f666db93e952aacee400e618e4c
Matt Surabian <matt@mattsurabian.com>
2018-09-08 22:07:05 -0400

    Update to use new gulp api

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var glob = require('glob');
 var shell = require('gulp-shell');
 var fs = require('fs');
 
-gulp.task('audio', function() {
+gulp.task('audio', gulp.parallel(function() {
   var files = glob.sync('./src/assets/sounds/*.mp3');
   var outputPath = './dist/audio';
   var opts = {
@@ -22,28 +22,28 @@ gulp.task('audio', function() {
 
     return fs.writeFile('./dist/audio' + '.json', JSON.stringify(obj, null, 2));
   });
-});
+}));
 
-gulp.task('images', function(){
+gulp.task('images', gulp.parallel(function(){
   // There is a texturepacker template for spritesmith but it doesn't work
   // well with complex directory structures, so instead we use the shell
   // checking TexturePacker --version first ensures it bails if TexturePacker
   // isn't installed
-  return gulp.src('', {read:false})
+  return gulp.src('*', {read:false})
     .pipe(shell([
       'TexturePacker --version || echo ERROR: TexturePacker not found, install TexturePacker',
       'TexturePacker --disable-rotation --data dist/sprites.json --format json --sheet dist/sprites.png src/assets/images'
     ]))
     .pipe(connect.reload());
-});
+}));
 
-gulp.task('deploy', function() {
-  return gulp.src('', {read:false})
+gulp.task('deploy', gulp.parallel(function() {
+  return gulp.src('*', {read:false})
     .pipe(shell([
     'AWS_PROFILE=duckhunt terraform plan',
     'AWS_PROFILE=duckhunt terraform apply',
     'aws --profile duckhunt s3 sync dist/ s3://duckhuntjs.com --include \'*\' --acl \'public-read\''
   ]));
-});
+}));
 
-gulp.task('default', ['images', 'audio']);
+gulp.task('default', gulp.parallel('images', 'audio'));
